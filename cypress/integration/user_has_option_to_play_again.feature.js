724060e9be5b1cb4af2ea9eebb6e4b3e825f5358
designerofthing <designerofthings@gmail.com>
2020-05-07 22:38:30 +0200

    adds user has option to play again feature test

--- cypress/integration/user_has_option_to_play_again.feature.js
+++ cypress/integration/user_has_option_to_play_again.feature.js
@@ -1,5 +1,5 @@
-describe('Game has an outcome', () => {
-  it('Allows the user to play the game against opponent and have an outcome', () => {
+describe('User has option to play again', () => {
+  it('Allows the user to play the game again', () => {
     cy.visit('http://localhost:3000'), 
     cy.window().then(win => {
     cy.stub(win, 'prompt').returns('rock')
@@ -9,7 +9,7 @@ describe('Game has an outcome', () => {
     cy.get('button#play').click();
     cy.contains('Rock').click('left','Rock');
     cy.get('button#play').click();
-    cy.get('#outcome').should('contain', 'You')
+    cy.get('#outcome').should('contain', 'Click Play to Play Again')
   })
   })
 })
